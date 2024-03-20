64f10458882bf313b73e6e75e4dce8e837aeba0f
servo pcerda <fernandonando98@live.com.mx>
2024-03-19 20:54:07 -0600

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jwt-simple';
 
 const register = async (req, res) => {
   try {
@@ -27,6 +28,56 @@ const register = async (req, res) => {
   }
 };
 
-const login = (req, res) => {};
+const login = async (req, res) => {
+  //Pedimos password y correo
+  if (!req.body.password || !req.body.email) {
+    return res.status(400).json({
+      msg: 'Bad login',
+    });
+  }
+
+  try {
+    //Buscamos user con ese correo
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+
+    //Si no hay user regresamos un 404
+    if (!user) {
+      return res.status(404).json({
+        msg: 'Usuario no encontrado',
+      });
+    }
+
+    //Comparar contraseñas
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (isPasswordCorrect) {
+      //crear un token
+      const payload = {
+        email: user.email,
+        role: user.role,
+      };
+      const token = jwt.encode(payload, process.env.SECRET);
+      //regresar un token
+      return res.json({
+        msg: 'Login correcto',
+        token,
+      });
+    } else {
+      return res.status(401).json({
+        msg: 'Datos incorrectos',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      msg: 'Error al hacer login',
+      error,
+    });
+  }
+};
 
 export { register, login };
